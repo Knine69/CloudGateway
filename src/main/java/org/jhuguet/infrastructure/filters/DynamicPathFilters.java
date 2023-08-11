78f25522375e9323b7f742243844c28ca441d97f
juan.huguet2 <juan.huguet@pragma.com.co>
2023-08-11 17:37:03 -0500

    REFACTOR: changed incorrectly used metadata and innerclass

--- src/main/java/org/jhuguet/infrastructure/filters/DynamicPathFilters.java
+++ src/main/java/org/jhuguet/infrastructure/filters/DynamicPathFilters.java
@@ -5,36 +5,20 @@ import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFac
 import org.springframework.stereotype.Component;
 
 @Component
-public class DynamicPathFilters extends AbstractGatewayFilterFactory<DynamicPathFilters.Config> {
-
-    public DynamicPathFilters() {
-        super(Config.class);
-    }
+public class DynamicPathFilters extends AbstractGatewayFilterFactory<DynamicPathFilters> {
 
     @Override
-    public GatewayFilter apply(Config config) {
+    public GatewayFilter apply(DynamicPathFilters config) {
         return (exchange, chain) -> {
-            String pathVariable = exchange.getAttribute(config.getPathVariable());
-            exchange.getAttributes()
-                    .put("pathVariable", pathVariable);
+            String variable = exchange.getRequest()
+                    .getPath()
+                    .value();
             exchange.getRequest()
                     .mutate()
-                    .path("/" + pathVariable)
+                    .path("/" + variable)
                     .build();
             return chain.filter(exchange);
         };
+
     }
-
-    public static class Config {
-        private String pathVariable;
-
-        public String getPathVariable() {
-            return pathVariable;
-        }
-
-        public void setPathVariable(String pathVariable) {
-            this.pathVariable = pathVariable;
-        }
-    }
-
 }
